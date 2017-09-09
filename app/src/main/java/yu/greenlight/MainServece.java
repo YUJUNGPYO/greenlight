4138eded060a4e8a1a6211aeec5eac6f2634f301
yujungpyo <burned0127@gmail.com>
2017-09-09 14:40:12 +0900

    Initial commit

---
+++ app/src/main/java/yu/greenlight/MainServece.java
@@ -0,0 +1,16 @@
+package yu.greenlight;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MainServece extends Service {
+    public MainServece() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
